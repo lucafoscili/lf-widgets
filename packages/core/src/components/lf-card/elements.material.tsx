3e53ed71da916d3b829f20c5ba34fd5a28aa7ff2
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2025-02-14 19:45:19 +0100

    fix(card): fixed unscrollable description

--- packages/core/src/components/lf-card/elements.material.tsx
+++ packages/core/src/components/lf-card/elements.material.tsx
@@ -1,8 +1,15 @@
-import { LfCardAdapter } from "@lf-widgets/foundations";
+import { LfCardAdapter, LfDataCell } from "@lf-widgets/foundations";
 import { h, VNode } from "@stencil/core";
+import { LfShape } from "../../utils/shapes";
 import { LfCard } from "./lf-card";
 
 //#region Material layout
+/**
+ * Prepares the material layout for the LfCard component.
+ *
+ * @param {() => LfCardAdapter} getAdapter - Function to get the LfCardAdapter instance.
+ * @returns {VNode} The virtual node representing the material layout.
+ */
 export const prepMaterial = (getAdapter: () => LfCardAdapter): VNode => {
   const {
     blocks,
@@ -15,8 +22,7 @@ export const prepMaterial = (getAdapter: () => LfCardAdapter): VNode => {
     shapes,
   } = getAdapter().controller.get;
   const { material } = defaults;
-  const { data, effects, theme } = manager;
-  const { decorate } = data.cell.shapes;
+  const { effects, theme } = manager;
   const { bemClass } = theme;
   const { materialLayout, textContent } = blocks;
 
@@ -24,31 +30,62 @@ export const prepMaterial = (getAdapter: () => LfCardAdapter): VNode => {
   const comp = compInstance as LfCard;
 
   //#region Button
-  const buttons = decorate("button", button, async (e) =>
-    comp.onLfEvent(e, "lf-event"),
-  );
+  const buttons: LfDataCell<"button">[] = [];
+  for (let index = 0; index < button.length; index++) {
+    buttons.push(
+      <LfShape
+        shape={"button"}
+        cell={button[index]}
+        index={index}
+        eventDispatcher={async (e) => comp.onLfEvent(e, "lf-event")}
+        framework={manager}
+      ></LfShape>,
+    );
+  }
   const hasButton = Boolean(buttons?.length);
   //#endregion
 
   //#region Image
-  const images = decorate(
-    "image",
-    image,
-    async (e) => comp.onLfEvent(e, "lf-event"),
-    material.image(),
-  );
+  const images: LfDataCell<"image">[] = [];
+  const imagesDef = material.image();
+  for (let index = 0; index < image.length; index++) {
+    images.push(
+      <LfShape
+        shape={"image"}
+        cell={
+          imagesDef[index]
+            ? Object.assign(imagesDef[index], image[index])
+            : image[index]
+        }
+        index={index}
+        eventDispatcher={async (e) => comp.onLfEvent(e, "lf-event")}
+        framework={manager}
+      ></LfShape>,
+    );
+  }
   const hasImage = Boolean(images?.length);
   //#endregion
 
   //#region Text
-  const texts = decorate("text", text, async (e) =>
-    comp.onLfEvent(e, "lf-event"),
-  );
+  const texts: LfDataCell<"text">[] = [];
+  for (let index = 0; index < text.length; index++) {
+    texts.push(
+      <LfShape
+        shape={"text"}
+        cell={text[index]}
+        index={index}
+        eventDispatcher={async (e) => comp.onLfEvent(e, "lf-event")}
+        framework={manager}
+      ></LfShape>,
+    );
+  }
   const hasText = Boolean(texts?.length);
   const title = (hasText && text?.[0]?.value) || null;
   const subtitle = (hasText && text?.[1]?.value) || null;
   const description = (hasText && text?.[2]?.value) || null;
   //#endregion
+
+  let rippleSurface: HTMLDivElement;
 
   return (
     <div
@@ -56,6 +93,9 @@ export const prepMaterial = (getAdapter: () => LfCardAdapter): VNode => {
         "has-actions": hasButton,
       })}
       data-lf={lfAttributes[comp.lfUiState]}
+      onPointerDown={(e) => {
+        effects.ripple(e as PointerEvent, rippleSurface);
+      }}
       part={parts.materialLayout}
     >
       {hasImage && (
@@ -81,14 +121,11 @@ export const prepMaterial = (getAdapter: () => LfCardAdapter): VNode => {
       <div
         data-cy={cyAttributes.rippleSurface}
         data-lf={lfAttributes.rippleSurface}
-        onPointerDown={(e) => {
-          effects.ripple(e as PointerEvent, e.currentTarget as HTMLElement);
-        }}
-        part={lfAttributes.rippleSurface}
+        ref={(r: HTMLDivElement) => (rippleSurface = r)}
       ></div>
       {hasButton && (
         <div class={bemClass(materialLayout._, materialLayout.actionsSection)}>
-          {...buttons}
+          {buttons}
         </div>
       )}
     </div>
